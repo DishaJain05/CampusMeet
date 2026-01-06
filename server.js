30162a77fbc4c7f20c308a60ce5dc0acd8b58458
DishaJain05 <141540874+DishaJain05@users.noreply.github.com>
2026-01-06 22:08:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,135 +1,28 @@
-// const express = require("express");
-// const http = require("http");
-// const { Server } = require("socket.io");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// app.use(express.static("public"));
-
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
-
-//   socket.on("join-room", (roomId) => {
-//     socket.join(roomId);
-//     socket.to(roomId).emit("user-joined", socket.id);
-//   });
-
-//   socket.on("signal", ({ to, data }) => {
-//     io.to(to).emit("signal", {
-//       from: socket.id,
-//       data
-//     });
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-//   });
-// });
-
-// server.listen(3000, () => {
-//   console.log("Server running at http://localhost:3000");
-// });
-
-
-
-//code 2
-
-// const express = require("express");
-// const http = require("http");
-// const { Server } = require("socket.io");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// app.use(express.static("public"));
-
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
-
-//   socket.on("join-room", (roomId) => {
-//     socket.join(roomId);
-//     socket.to(roomId).emit("user-joined", socket.id);
-//   });
-
-//   socket.on("signal", ({ to, data }) => {
-//     io.to(to).emit("signal", {
-//       from: socket.id,
-//       data
-//     });
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-//   });
-// });
-
-// server.listen(3000, () => {
-//   console.log("Server running at http://localhost:3000");
-// });
-
-
-
-
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 
 const app = express();
+const wss = new WebSocket.Server({ port: 3000 });
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+let clients = [];
 
-app.use(express.static("public"));
+wss.on("connection", ws => {
+  clients.push(ws);
 
-// roomId -> clients
-const rooms = new Map();
-
-wss.on("connection", (ws) => {
-  ws.roomId = null;
-
-  ws.on("message", (message) => {
-    const msg = JSON.parse(message);
-
-    // Join room
-    if (msg.type === "join") {
-      ws.roomId = msg.roomId;
-
-      if (!rooms.has(ws.roomId)) {
-        rooms.set(ws.roomId, new Set());
+  ws.on("message", message => {
+    clients.forEach(client => {
+      if (client !== ws) {
+        client.send(message);
       }
-
-      rooms.get(ws.roomId).add(ws);
-
-      // notify others
-      rooms.get(ws.roomId).forEach(client => {
-        if (client !== ws) {
-          client.send(JSON.stringify({ type: "user-joined" }));
-        }
-      });
-    }
-
-    // WebRTC signaling
-    if (msg.type === "signal") {
-      rooms.get(ws.roomId)?.forEach(client => {
-        if (client !== ws) {
-          client.send(JSON.stringify({
-            type: "signal",
-            data: msg.data
-          }));
-        }
-      });
-    }
+    });
   });
 
   ws.on("close", () => {
-    if (ws.roomId && rooms.has(ws.roomId)) {
-      rooms.get(ws.roomId).delete(ws);
-    }
+    clients = clients.filter(c => c !== ws);
   });
 });
 
-server.listen(3000, () => {
-  console.log("WebSocket server running on http://localhost:3000");
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
